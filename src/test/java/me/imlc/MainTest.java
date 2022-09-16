9facdf032e3f57fea6c123f9cff7aff89f64323b
Lawrence Ching <lawrence.ching.zh@gmail.com>
2022-09-16 23:53:12 +0800

    Support native image build

---
+++ src/test/java/me/imlc/MainTest.java
@@ -0,0 +1,22 @@
+package me.imlc;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmc.flightrecorder.CouldNotLoadRecordingException;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void jfr() throws CouldNotLoadRecordingException, IOException, InterruptedException {
+        Main.main(
+                new String[] {
+                        "--from",
+                        "native-image/jetty.jfr",
+                        "--to",
+                        "target/jetty.html"
+                }
+        );
+    }
+}
